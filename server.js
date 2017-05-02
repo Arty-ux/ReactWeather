60ea3f5ac0b6402c9a6cb48bad2e84542ffe1f72
AtomRise <artiomsobolev@gmail.com>
2017-05-02 21:16:05 +0100

    add Foundation and Jquery

--- server.js
+++ server.js
@@ -3,18 +3,26 @@ var express = require('express');
 // Create our app
 
 var app = express();
-const PORT = process.evn.PORT || 3000;
-
-app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
-    res.redirect('http://'+ req.hostname + req.url);
-  }
-});
 
 app.use(express.static('public'));
+app.listen(3000, function () {
+  console.log('its running on port 3000')
+})
 
-app.listen(PORT, function () {
-  console.log('its running on port' + PORT)
-});
+
+// Heroku setup
+// const PORT = process.evn.PORT || 3000;
+//
+// app.use(function (req, res, next){
+//   if (req.headers['x-forwarded-proto'] === 'https') {
+//     res.redirect('http://'+ req.hostname + req.url);
+//   } else {
+//     next();
+//   }
+// });
+//
+// app.use(express.static('public'));
+//
+// app.listen(PORT, function () {
+//   console.log('its running on port' + PORT)
+// });
