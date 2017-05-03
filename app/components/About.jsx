0ad46c93d00723cdd0f65aef1a28ad3b563f6388
AtomRise <artiomsobolev@gmail.com>
2017-05-03 18:13:32 +0100

    about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,17 @@ var React = require('react');
 
 var About = (props) => {
   return (
-     <h3>About component</h3>
+     <div>
+       <h1 className="text-center">About</h1>
+       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse velit leo, iaculis at nibh vitae, maximus semper nunc. Fusce sed turpis euismod, porta eros at, gravida libero. Nunc lobortis tincidunt purus, nec rhoncus eros convallis fermentum. Vivamus mollis sollicitudin sapien at elementum. Aliquam volutpat mauris nec ipsum ultrices, sit amet venenatis risus molestie. Cras vehicula, metus non mollis suscipit, risus dolor consectetur ligula, consectetur auctor libero felis vel diam. Proin sed massa vel eros volutpat finibus. Vivamus vestibulum nibh eu dui interdum rutrum.</p>
+       <p>
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse velit leo, iaculis at nibh vitae, maximus semper nunc. Fusce sed turpis euismod, porta eros at, gravida libero. Nunc lobortis tincidunt purus, nec rhoncus eros convallis fermentum. Vivamus mollis sollicitudin sapien at elementum. Aliquam volutpat mauris nec ipsum ultrices, sit amet venenatis risus molestie. Cras vehicula, metus non mollis suscipit, risus dolor consectetur ligula, consectetur auctor libero felis vel diam. Proin sed massa vel eros volutpat finibus. Vivamus vestibulum nibh eu dui interdum rutrum.
+       </p>
+       <p>
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse velit leo, iaculis at nibh vitae, maximus semper nunc. Fusce sed turpis euismod, porta eros at, gravida libero. Nunc lobortis tincidunt purus, nec rhoncus eros convallis fermentum. Vivamus mollis sollicitudin sapien at elementum. Aliquam volutpat mauris nec ipsum ultrices, sit amet venenatis risus molestie. Cras vehicula, metus non mollis suscipit, risus dolor consectetur ligula, consectetur auctor libero felis vel diam. Proin sed massa vel eros volutpat finibus. Vivamus vestibulum nibh eu dui interdum rutrum.
+       </p>
+       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse velit leo, iaculis at nibh vitae, maximus semper nunc. Fusce sed turpis euismod, porta eros at, gravida libero. Nunc lobortis tincidunt purus, nec rhoncus eros convallis fermentum. Vivamus mollis sollicitudin sapien at elementum. Aliquam volutpat mauris nec ipsum ultrices, sit amet venenatis risus molestie. Cras vehicula, metus non mollis suscipit, risus dolor consectetur ligula, consectetur auctor libero felis vel diam. Proin sed massa vel eros volutpat finibus. Vivamus vestibulum nibh eu dui interdum rutrum.</p>
+   </div>
   )
 };
 
