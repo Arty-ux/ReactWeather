96b93a7fc59442c35fc0d7c61064d8e5b4dd6a69
vagrant <vagrant@drupalvm.dev>
2017-06-29 18:25:02 +0000

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -3,26 +3,19 @@ var express = require('express');
 // Create our app
 
 var app = express();
+const PORT = process.evn.PORT || 3000;
+
+
+app.use(function (req, res, next){
+   if (req.headers['x-forwarded-proto'] === 'https') {
+     res.redirect('http://'+ req.hostname + req.url);
+   } else {
+     next();
+  }
+});
 
 app.use(express.static('public'));
-app.listen(3000, function () {
-  console.log('its running on port 3000')
-})
 
-
-// Heroku setup
-// const PORT = process.evn.PORT || 3000;
-//
-// app.use(function (req, res, next){
-//   if (req.headers['x-forwarded-proto'] === 'https') {
-//     res.redirect('http://'+ req.hostname + req.url);
-//   } else {
-//     next();
-//   }
-// });
-//
-// app.use(express.static('public'));
-//
-// app.listen(PORT, function () {
-//   console.log('its running on port' + PORT)
-// });
+app.listen(PORT, function () {
+   console.log('its running on port' + PORT)
+});
